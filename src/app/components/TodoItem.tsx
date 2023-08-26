b3745cfc72e4792fac0a5418d0edc5173a6dc7d3
Leon Ampah <l.ampah@akofisgroup.com>
2023-08-26 23:48:46 +0000

    Completed Todo app with Next JS and prisma

---
+++ src/app/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+type TodoItemProps = {
+    id: string,
+    title: string,
+    complete: boolean,
+    toggleTodo: (id: string, complete: boolean) => void
+}
+
+export const TodoItem = ({ id, title, complete, toggleTodo }: TodoItemProps) => {
+    return <li className="flex gap-1 items-center">
+        <input onChange={(e) => toggleTodo(id, e.target.checked)} defaultChecked={complete} id={id} type="checkbox" className="cursor-pointer peer" />
+        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+    </li>
+}
