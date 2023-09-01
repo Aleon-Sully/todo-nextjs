5e3e1502df82ee4d7d70054019e378237d03061e
ti908 <martinwalterampah@gmail.com>
2023-09-01 23:08:21 +0000

    i added an h1 tag to the code

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -18,6 +18,7 @@ const Page = () => {
     return <>
         <header className="flex justify-between items-center mb-4">
             <h1 className="text-2xl">New</h1>
+            <h1>martin</h1>
         </header>
         <form className="flex gap-2 flex-col" action={createTodo}>
             <input type="text" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100" />
